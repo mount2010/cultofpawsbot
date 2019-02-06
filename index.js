43ba623f5948b6dc5318e3267dd9237439877dc6
mount2010 <mount201076@gmail.com>
2019-02-06 17:38:28 +0000

    add prettier

--- index.js
+++ index.js
@@ -5,30 +5,33 @@ const secrets = require("./secrets.json");
 Logger.useDefaults();
 
 class COPBotClient extends Discord.Client {
-	constructor () {
+	constructor() {
 		super();
 		this.messageListening = [];
 		this.setupEvents();
 	}
-	setupEvents () {
-		this.on("ready", ()=>{
+	setupEvents() {
+		this.on("ready", () => {
 			Logger.info(`Ready as ${this.user.username}!`);
 		});
-		this.on("message", (msg)=>{
-			this.messageListening.forEach((el) => {el(msg);});
+		this.on("message", msg => {
+			this.messageListening.forEach(el => {
+				el(msg);
+			});
 		});
 	}
-	login (token) {
+	login(token) {
 		super.login(token);
 	}
-	listenForMessages (func) {
+	listenForMessages(func) {
 		this.messageListening.push(func);
 	}
 }
 
 const client = new COPBotClient();
 client.login(secrets.token);
-client.listenForMessages((msg) => {if (msg.content == "meow") msg.channel.send("yeah");});
+client.listenForMessages(msg => {
+	if (msg.content == "meow") msg.channel.send("yeah");
+});
 
 module.exports = client;
-
--- start.sh
+++ start.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-eslint .
-if [[ $? == 0 || $force == "true" ]]; then 
+function runBot {
 	node index.js
+	if [[ $? != 0 ]]; then
+		echo -e "\033[31;1mThe bot has failed. Please fix the issue and restart it.\033[0m"
+		exit 1
+	fi
+}
+
+
+#function checkOutput {
+#	"$@"
+#	code=$?
+#	if [[ $code != 0 ]]; then
+#		echo -e "\033[31;1m$1 failed, refusing to run bot. (Prefix force=true to force a run.)\033[0m"
+#		exit 1
+#	fi
+#	return $code
+#}
+eslint .
+eslintOutput=$?
+prettier --check "*.js"
+prettierOutput=$?
+
+doRun=$(($eslintOutput + $prettierOutput))
+if [[ $force == true ]]; then
+	runBot
+elif [[ $doRun == 0 ]]; then
+	runBot
 else
-	echo -e "\033[31;1meslint linting failed! Bot will not start. (Prefix force=true to bypass.)\033[0m"
-fi	
+	echo -e "\033[31;1mAll checks failed, refusing to run bot. (Prefix force=true to force a run.)\033[0m"
+	exit 1
+fi
